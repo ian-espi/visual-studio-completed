078282503f5848130aef8d35c0e7335cee8aaaa7
ian-espi <78582556+ian-espi@users.noreply.github.com>
2021-02-05 00:00:58 -0800

    Add files via upload
    These are files for the a GUI I made using Visual Basic in Visual Studio.

---
+++ frmPizzaSelection.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.picDeepDish = New System.Windows.Forms.PictureBox()
+        Me.picThinCrust = New System.Windows.Forms.PictureBox()
+        Me.btnDeepDish = New System.Windows.Forms.Button()
+        Me.btnThinCrust = New System.Windows.Forms.Button()
+        Me.btnSelectPizza = New System.Windows.Forms.Button()
+        Me.btnExitWindow = New System.Windows.Forms.Button()
+        Me.lblDescription = New System.Windows.Forms.Label()
+        Me.lblConfirmation = New System.Windows.Forms.Label()
+        CType(Me.picDeepDish, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picThinCrust, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.Color.Firebrick
+        Me.lblHeading.Location = New System.Drawing.Point(93, 13)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(226, 34)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Pizza Selection"
+        '
+        'picDeepDish
+        '
+        Me.picDeepDish.Image = CType(resources.GetObject("picDeepDish.Image"), System.Drawing.Image)
+        Me.picDeepDish.Location = New System.Drawing.Point(13, 45)
+        Me.picDeepDish.Name = "picDeepDish"
+        Me.picDeepDish.Size = New System.Drawing.Size(185, 150)
+        Me.picDeepDish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picDeepDish.TabIndex = 1
+        Me.picDeepDish.TabStop = False
+        Me.picDeepDish.Visible = False
+        '
+        'picThinCrust
+        '
+        Me.picThinCrust.Image = CType(resources.GetObject("picThinCrust.Image"), System.Drawing.Image)
+        Me.picThinCrust.Location = New System.Drawing.Point(215, 45)
+        Me.picThinCrust.Name = "picThinCrust"
+        Me.picThinCrust.Size = New System.Drawing.Size(185, 150)
+        Me.picThinCrust.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picThinCrust.TabIndex = 2
+        Me.picThinCrust.TabStop = False
+        Me.picThinCrust.Visible = False
+        '
+        'btnDeepDish
+        '
+        Me.btnDeepDish.BackColor = System.Drawing.Color.Wheat
+        Me.btnDeepDish.Location = New System.Drawing.Point(6, 202)
+        Me.btnDeepDish.Name = "btnDeepDish"
+        Me.btnDeepDish.Size = New System.Drawing.Size(94, 25)
+        Me.btnDeepDish.TabIndex = 3
+        Me.btnDeepDish.Text = "Deep Dish"
+        Me.btnDeepDish.UseVisualStyleBackColor = False
+        '
+        'btnThinCrust
+        '
+        Me.btnThinCrust.BackColor = System.Drawing.Color.Wheat
+        Me.btnThinCrust.Location = New System.Drawing.Point(312, 202)
+        Me.btnThinCrust.Name = "btnThinCrust"
+        Me.btnThinCrust.Size = New System.Drawing.Size(94, 25)
+        Me.btnThinCrust.TabIndex = 4
+        Me.btnThinCrust.Text = "Thin Crust"
+        Me.btnThinCrust.UseVisualStyleBackColor = False
+        '
+        'btnSelectPizza
+        '
+        Me.btnSelectPizza.BackColor = System.Drawing.Color.Wheat
+        Me.btnSelectPizza.Enabled = False
+        Me.btnSelectPizza.Location = New System.Drawing.Point(158, 202)
+        Me.btnSelectPizza.Name = "btnSelectPizza"
+        Me.btnSelectPizza.Size = New System.Drawing.Size(94, 25)
+        Me.btnSelectPizza.TabIndex = 5
+        Me.btnSelectPizza.Text = "Select Pizza"
+        Me.btnSelectPizza.UseVisualStyleBackColor = False
+        '
+        'btnExitWindow
+        '
+        Me.btnExitWindow.BackColor = System.Drawing.Color.Wheat
+        Me.btnExitWindow.Enabled = False
+        Me.btnExitWindow.Location = New System.Drawing.Point(161, 314)
+        Me.btnExitWindow.Name = "btnExitWindow"
+        Me.btnExitWindow.Size = New System.Drawing.Size(94, 25)
+        Me.btnExitWindow.TabIndex = 6
+        Me.btnExitWindow.Text = "Exit"
+        Me.btnExitWindow.UseVisualStyleBackColor = False
+        '
+        'lblDescription
+        '
+        Me.lblDescription.AutoSize = True
+        Me.lblDescription.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescription.Location = New System.Drawing.Point(12, 243)
+        Me.lblDescription.Name = "lblDescription"
+        Me.lblDescription.Size = New System.Drawing.Size(394, 18)
+        Me.lblDescription.TabIndex = 7
+        Me.lblDescription.Text = "Choose a Pizza Type and then Click the Select Pizza Button"
+        '
+        'lblConfirmation
+        '
+        Me.lblConfirmation.AutoSize = True
+        Me.lblConfirmation.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConfirmation.Location = New System.Drawing.Point(121, 270)
+        Me.lblConfirmation.Name = "lblConfirmation"
+        Me.lblConfirmation.Size = New System.Drawing.Size(178, 18)
+        Me.lblConfirmation.TabIndex = 8
+        Me.lblConfirmation.Text = "Enjoy Your Pizza Selection"
+        Me.lblConfirmation.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(412, 348)
+        Me.Controls.Add(Me.lblConfirmation)
+        Me.Controls.Add(Me.lblDescription)
+        Me.Controls.Add(Me.btnExitWindow)
+        Me.Controls.Add(Me.btnSelectPizza)
+        Me.Controls.Add(Me.btnThinCrust)
+        Me.Controls.Add(Me.btnDeepDish)
+        Me.Controls.Add(Me.picThinCrust)
+        Me.Controls.Add(Me.picDeepDish)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "Form1"
+        Me.Text = "Pizza Selection"
+        CType(Me.picDeepDish, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picThinCrust, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents picDeepDish As PictureBox
+    Friend WithEvents picThinCrust As PictureBox
+    Friend WithEvents btnDeepDish As Button
+    Friend WithEvents btnThinCrust As Button
+    Friend WithEvents btnSelectPizza As Button
+    Friend WithEvents btnExitWindow As Button
+    Friend WithEvents lblDescription As Label
+    Friend WithEvents lblConfirmation As Label
+End Class
---
+++ frmPizzaSelection.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles picDeepDish.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblConfirmation.Click
+
+    End Sub
+
+    Private Sub btnDeepDish_Click(sender As Object, e As EventArgs) Handles btnDeepDish.Click
+        picDeepDish.Visible = True
+        picThinCrust.Visible = False
+        btnSelectPizza.Enabled = True
+    End Sub
+
+    Private Sub btnExitWindow_Click(sender As Object, e As EventArgs) Handles btnExitWindow.Click
+        Close()
+    End Sub
+
+    Private Sub btnSelectPizza_Click(sender As Object, e As EventArgs) Handles btnSelectPizza.Click
+        btnDeepDish.Enabled = False
+        btnSelectPizza.Enabled = False
+        btnThinCrust.Enabled = False
+        lblDescription.Visible = False
+        lblConfirmation.Visible = True
+        btnExitWindow.Enabled = True
+    End Sub
+
+    Private Sub btnThinCrust_Click(sender As Object, e As EventArgs) Handles btnThinCrust.Click
+        picThinCrust.Visible = True
+        picDeepDish.Visible = False
+        btnSelectPizza.Enabled = True
+    End Sub
+End Class
